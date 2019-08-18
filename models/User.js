f675a3fbe3e810213e9815541fd7b40aecd8e4a4
Jezreel <jezreel.go@gmail.com>
2019-08-18 16:13:30 -0700

    add bcrypt

--- models/User.js
+++ models/User.js
@@ -1,8 +1,28 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
     email: { type: String, require: true, unique: true },
     password: { type: String, required: true }
+})
+
+UserSchema.pre('save', function(next) {
+    if (this.isNew || this.isModified('password') {
+        const document = this;
+        bcrypt.hash(document.password, saltRounds,
+            function(err, hashedPassword) {
+                if (err) {
+                    next(err);
+                } else {
+                    document.password = hashedPassword;
+                    next();
+                }
+        });
+    } else {
+        next();
+    }
 })
 
 module.exports = mongoose.model('User', UserSchema);
