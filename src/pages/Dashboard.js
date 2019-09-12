915a710f81485262dd8d3d055254c677ca5cd7d1
Adrienne Hein <adrienne.hein@gmail.com>
2019-09-11 17:18:33 -0700

    Simple tasks can be added to dashboard. Not saving to DB

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
+import TaskList from './TaskList';
 
 const Div = styled.div`
   ${props => props.main && css `
@@ -26,53 +27,40 @@ export default class Dashboard extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      currentTask: '',
       tasks: [],
-      task: {
-        taskTitle: '',
-        taskDescription: '',
-        completed: false,
-      },
     }
   }
 
   handleChange = (event) => {
     const { value, name } = event.target;
-    const task = { [name]: value };
     this.setState({
-      task: task,
+      [name]: value,
     })
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const newTask = this.state.task;
-    console.log(newTask);
+    this.setState({
+      currentTask: '',
+      tasks: [...this.state.tasks, this.state.currentTask]
+    })
+    console.log(this.state);
   }
     render() {
+      const { currentTask } = this.state;
         return(
           <Div main>
             <Div container>
               <h1>Dashboard</h1>
-              <section>
-                <h3>Active Tasks</h3>
-                <Div taskCard>
-                  <h4>Water the Dog</h4>
-                  <p>The dog needs water to grow.</p>
-                </Div>
-                <Div taskCard>
-                  <h4>Feed the dog to the plant</h4>
-                  <p>The plant is hungry.</p>
-                </Div>
                 <Div taskCard>
                   <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="taskTitle">Task Title</label>
-                    <input name="taskTitle" id="taskTitle" onChange={this.handleChange}/>
-                    <label htmlFor="taskDescription">Description of task</label>
-                    <input name="taskDescription" id="taskDescription" onChange={this.handleChange}/>
+                    <label htmlFor="currentTask">Description of task</label>
+                    <input name="currentTask" id="currentTask" onChange={this.handleChange} value={currentTask}/>
                     <input type="submit" value="Add Task" />
                   </form>
                 </Div>
-              </section>
+                <TaskList tasks={this.state.tasks}/>
               <section>
                 <h3>Completed Tasks</h3>
                 <Div taskCard>
